d49fc653b82c4f3a8d41248d143063303b2c7a8f
StrockLee <weangtsai@gmail.com>
2024-11-16 20:30:39 +1000

    Add get_btc_trading_list func

--- api_trading.py
+++ api_trading.py
@@ -1,10 +1,11 @@
 from dotenv import load_dotenv
 import os
 from binance.client import Client
+
 load_dotenv()  # read from local .env file
 # Test
-api_key = os.environ['BINANCE_API_KEY_TEST']
-api_secret = os.environ['BINANCE_API_SECRET_TEST']
+api_key = os.environ["BINANCE_API_KEY_TEST"]
+api_secret = os.environ["BINANCE_API_SECRET_TEST"]
 client = Client(api_key, api_secret, testnet=True)
 
 # Live trading
@@ -12,8 +13,6 @@ client = Client(api_key, api_secret, testnet=True)
 # api_secret = os.environ['BINANCE_API_SECRET_LIVE']
 # client = Client(api_key, api_secret)
 
-buy_order = client.create_test_order(symbol='BTCUSDT',
-                                      side='BUY',
-                                      type='MARKET',
-                                      quantity=0.05
-                                     )
+buy_order = client.create_test_order(
+    symbol="BTCUSDT", side="BUY", type="MARKET", quantity=0.05
+)
